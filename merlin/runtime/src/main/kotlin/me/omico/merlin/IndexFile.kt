2a727f06910055ccafa3fdc1a2599033b7b47c62
Omico <me@omico.icu>
2023-08-15 14:54:48 -0700

    runtime: Simplify getting available indexes

--- merlin/runtime/src/main/kotlin/me/omico/merlin/IndexFile.kt
+++ merlin/runtime/src/main/kotlin/me/omico/merlin/IndexFile.kt
@@ -3,7 +3,13 @@
 package me.omico.merlin
 
 @JvmInline
-value class IndexFile(val name: String) {
+value class IndexFile(val id: String) {
+    val name: String
+        get() = "$id-index"
+
     val hash: String
         get() = "$name.hash"
+
+    val json: String
+        get() = "$name.json"
 }
---
+++ merlin/runtime/src/main/kotlin/me/omico/merlin/IndexFiles.kt
@@ -0,0 +1,8 @@
+// Copyright 2023 Omico
+// SPDX-License-Identifier: Apache-2.0
+package me.omico.merlin
+
+object IndexFiles {
+    val Google: IndexFile = IndexFile("google")
+    val All: Set<IndexFile> = setOf(Google)
+}
