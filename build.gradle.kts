1bb38b4ca4dd460c4a93a9e99d097311072104da
Omico <me@omico.icu>
2023-08-15 15:53:55 -0700

    Simplified execution of deployment tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,11 @@
 plugins {
     id("merlin.root")
 }
+
+val deployIndexes by tasks.registering {
+    group = "merlin"
+    dependsOn(
+        ":merlin-google:run",
+        ":merlin-omico:run",
+    )
+}
